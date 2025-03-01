4f08611f85a2d80df8dfa5833bb3f0bf1f8c6800
Imran <isoteriksoftware@gmail.com>
2025-03-01 19:34:47 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { EsLinter, linterPlugin } from "vite-plugin-linter";
+import * as packageJson from "./package.json";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ["./src}/**/*.{ts,tsx}"],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ["src/components", "src/core", "src/index.ts"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "index.ts"),
+      name: "react-word-cloud",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-word-cloud.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: [...Object.keys(packageJson.peerDependencies)],
+  },
+}));
